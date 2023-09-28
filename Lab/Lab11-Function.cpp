dec2fe6cc669f9294cb19a432e715f4ec7d962fc
tatar008 <jirapat_ja66@live.rumtl.ac.th>
2023-09-28 15:12:05 +0700

    lab11

--- Lab/Lab11-Function.cpp
+++ Lab/Lab11-Function.cpp
@@ -1,3 +1,29 @@
+/*
+    จงเขียนฟังก์ชันในการตรวจสอบตัวเลขที่ผู้ใช้ป้อนเข้ามา ว่าเป็นตัวเลขที่เป็นอาร์มสตรองหรือไม่ หากใช่ให้แสดงคำว่า Pass หากไม่ใช่แสดงคำว่า Not Pass
+    
+    หมายเหตุ: ตัวเลขอาร์มสตรอง คือ ตัวเลขที่ผลรวมของเลขยกกำลังของจำนวนหลัก เช่น
+    153 เป็นตัวเลขอาร์มสตรอง เพราะ 153 มีทั้งหมด 3 หลัก จึงมีค่าเท่ากับ = 1^3 +  5^3 +  3^3 (โดยที่เครื่องหมาย ^ หมายถึงเลขยกกำลัง)
+    370 = 3^3 + 7^3 + 0^3
+
+    Test Case:
+        Enter Number:
+            153
+    Output:
+        Pass.
+    
+    Test Case:
+        Enter Number:
+            370
+    Output:
+        Pass.
+    
+    Test Case:
+        Enter Number:
+            372
+    Output:
+        Not Pass.
+*/
+
 #include <stdio.h>
 #include <math.h>
 
@@ -38,9 +64,9 @@ int main() {
 
     // เรียกใช้ฟังก์ชัน isArmstrong เพื่อตรวจสอบ
     if ( isArmstrong( number ) == 1 ) {
-        printf( "Pass\n" ) ;
+        printf( "Pass.\n" ) ;
     } else {
-        printf( "Not Pass\n" ) ;
+        printf( "Not Pass.\n" ) ;
     }
 
     return 0 ;
